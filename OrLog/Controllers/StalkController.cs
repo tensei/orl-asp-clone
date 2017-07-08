efe37e861ed463e10c4ac0325b9882ca91fb5eba
Tensei <davuttensei@gmail.com>
2017-07-08 12:36:31 +0200

    stalk bby

---
+++ OrLog/Controllers/StalkController.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace OrLog.Controllers
+{
+    [Route("stalk")]
+    public class StalkController : Controller
+    {
+        private readonly HttpClient _client;
+
+        public StalkController(HttpClient client)
+        {
+            _client = client;
+        }
+
+        // GET: Stalk
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Index(string channel, string nick)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                ViewBag.channel = channel;
+                ViewBag.nick = nick;
+                ViewBag.url = $"https://overrustlelogs.net/api/v1/{ViewBag.channel}/months.json";
+                var monthsjson = await _client.GetStringAsync($"https://overrustlelogs.net/api/v1/{ViewBag.channel}/months.json");
+                var months = JsonConvert.DeserializeObject<List<string>>(monthsjson);
+                ViewBag.months = months;
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        
+    }
+}
