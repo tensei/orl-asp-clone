8e41ccaeab86eb2c51781ac40a9f50f297abe52a
Tensei <davuttensei@gmail.com>
2017-07-09 10:33:27 +0200

    moved stuff around

---
+++ OrLog/wwwroot/js/orl.js
@@ -0,0 +1,10 @@
+﻿
+$(document).ready(function () {
+    $('.scrollspy').scrollSpy({
+        scrollOffset: 50
+    });
+    $('.table-of-contents').pushpin({
+        top: 0,
+        offset: 60
+    });
+});
